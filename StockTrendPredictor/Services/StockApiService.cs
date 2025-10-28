663e8e45053a054c13b23c08da9bd98d7879e782
Helmer Bergström <hebe2400@student.miun.se>
2025-10-28 12:46:14 +0100

    Skapar felhantering genom if-sats som kollar om data finns i apiResponse.TimeSeriesDaily. Returnerar tom lista om data inte finns. Påbörjar metod för att konvertera data till StockData-lista. Använder LINQ för att konvertera.

--- StockTrendPredictor/Services/StockApiService.cs
+++ StockTrendPredictor/Services/StockApiService.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.Net.Http.Json;
 using System.Text.Json;
+using Microsoft.ML.Transforms;
 using StockTrendPredictor.Models;
 
 namespace StockTrendPredictor.Services
@@ -20,6 +21,27 @@ namespace StockTrendPredictor.Services
             // Serialiserar data från API:et, för att göra om data till C#-objekt ist för JSON.
             var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
             StockApiResponse? apiResponse = JsonSerializer.Deserialize<StockApiResponse>(json, options);
+
+            // Kontroll om det finns data i API:et. Finns det inte körs if-satsen.
+            if (apiResponse?.TimeSeriesDaily == null)
+            {
+                Console.WriteLine("Kunde inte hämta data från API:et. Kontrollera API-nyckeln och försök igen!");
+                // Avslutar metoden helt. Returnerar en tom lista, eftersom koden ovan alltid får en giltlig uppsättning av listan.
+                return new List<StockData>();
+            }
+
+            // Konverterar dictionary till StockData-lista.
+            // ATT GÖRA: Sortera denna
+            var list = apiResponse.TimeSeriesDaily
+                .Select(kvp =>
+                {
+                    var Date = DateTime.Parse(kvp.Key);
+                    var d = kvp.Value;
+                    return new StockData
+                    {
+
+                    };
+                });
 
         }
     }
